659fb4d19d6e8a8f20462c2036013e616b88ee64
yumeow0122 <yumeow0122@gmail.com>
2022-12-01 15:43:58 +0800

    main: * add simple test strategy

--- src/main.py
+++ src/main.py
@@ -1,18 +1,44 @@
 import os
-import sys
 import talib
 
-from dotenv import load_dotenv
-from binance import Client
+import pandas as pd
 
 # own module
 from backtrader.Agent import Agent
-
-load_dotenv()
-client = Client(os.getenv('API_KEY'), os.getenv('API_SECRET'))
-
+from utils import util
 
 # Write your strategy here
 if __name__ == '__main__':
-    agent = Agent(100, 2, 1)
-    # agent.print_detail()
+    df = util.build_dataframe('ETHBUSD', '15m', '1 weeks')
+    agent = Agent(100, 2, 1, df)
+
+    slowk, slowd = talib.STOCH(
+        df['High'], df['Low'], df['Close'], fastk_period=9, slowk_period=5, slowd_period=4)
+
+    print('---------- Backtest Started ----------')
+    start = util.get_efficient_start(slowk)
+    agent.fill_unuse_place(start)
+    for idx in range(start, len(df['Close'])):
+        if agent.holding == 0:
+            if slowk[idx] > slowd[idx] and slowk[idx-1] < slowd[idx-1]:
+                agent.open_long_position(idx, 0.99)
+
+            elif slowk[idx] < slowd[idx] and slowk[idx-1] > slowd[idx-1]:
+                agent.open_short_position(idx, 0.99)
+
+        elif agent.holding > 0:
+            if slowk[idx] < slowd[idx] and slowk[idx-1] > slowd[idx-1]:
+                agent.close_long_position(idx, 1)
+                agent.open_short_position(idx, 0.99)
+
+        elif agent.holding < 0:
+            if slowk[idx] > slowd[idx] and slowk[idx-1] < slowd[idx-1]:
+                agent.close_short_position(idx, 1)
+                agent.open_long_position(idx, 0.99)
+
+        print('k: ', slowk[idx])
+        print('d: ', slowd[idx])
+        print('price:', df['Close'][idx])
+        agent.log(df['Close'][idx])
+
+    agent.analyze()
